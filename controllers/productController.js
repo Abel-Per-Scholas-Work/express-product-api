5d00ccdb8d06ae09cd03509680d050b431c361cd
belunatic <abelluvyou@yahoo.com>
2025-11-25 22:39:51 -0800

    add controller for get all product and post new product

---
+++ controllers/productController.js
@@ -0,0 +1,40 @@
+const Product = require("../models/productModel");
+
+/**
+ * GET /
+ * get all products
+ */
+
+const getAllProducts = async (req, res) => {
+	try {
+		const products = await Product.find();
+		if (!products) {
+			res.status(400).json({ message: "Invalid URL" });
+		}
+		res.json(products);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: "Error fetching product from server" });
+	}
+};
+
+/**
+ * POST /
+ * create a new product
+ */
+const createProduct = async (req, res) => {
+	try {
+		const product = await Product.create(req.body);
+		if (!product) {
+			res.status(400).json({ message: "Failed to create a new product" });
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: error.message });
+	}
+};
+
+module.exports = {
+	getAllProducts,
+	createProduct,
+};
